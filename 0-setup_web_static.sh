2e08642d207b198a34c74b88a0b1cc6fcc96f381
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-10 01:16:42 +0000

    [Fix] Add / location

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,15 +7,9 @@ sudo service nginx start
 sudo ufw allow 'Nginx HTTP'
 sudo mkdir -p /data/web_static/releases/test
 sudo mkdir -p /data/web_static/shared 
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | tee /data/web_static/releases/test/index.html
+echo "Hello my fish" | tee -a /data/web_static/releases/test/index.html
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown ubuntu:ubuntu -R /data/
-sudo sed -i "48i\\\n\tlocation hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
+sudo sed -i "48i\\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n" /etc/nginx/sites-available/default
 sudo service nginx restart
 exit 0
