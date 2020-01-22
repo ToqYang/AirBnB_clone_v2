3175182c3ba584cb77f7b5cf6789ec1c95bf3c9e
Santiago Yanguas <santitoya-2001@outlook.com>
2020-01-22 23:27:11 +0000

    [Fix] Other change

--- models/state.py
+++ models/state.py
@@ -4,7 +4,6 @@ from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Column, String
 import models
-from models.city import City
 
 class State(BaseModel, Base):
     """This is the class for State
